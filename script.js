a8439a535087b91aeba82df1ac96e84dc00ddda8
Maciek Sitkowski <msitkowski94@gmail.com>
2020-09-03 18:44:11 +0200

    Add basic layout setup with overflow working

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const {
-  json
+  json,
+  select
 } = d3;
+
+const width = 1000;
+const height = document.documentElement.clientHeight;
+
+const svg = select('svg')
+  .attr('height', height)
+  .attr('width', width)
+    .append('rect')
+      .attr('height', height)
+      .attr('width', width)
+      .attr('fill', '#ccc');
 
 // Fetch data
 json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json')
