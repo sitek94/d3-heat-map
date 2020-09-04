66416f12ce4684066376814630346d815e8589ef
Maciek Sitkowski <msitkowski94@gmail.com>
2020-09-04 12:05:29 +0200

    Add comments, cleanup and refactor code

--- script.js
+++ script.js
@@ -33,40 +33,32 @@ const render = (sourceData) => {
   const yValue = (d) => d.month;
 
   // Margins 
-  const margin = { top: 90, right: 20, bottom: 80, left: 100 };
+  const margin = { top: 90, right: 20, bottom: 80, left: 120 };
 
   // Inner dimensions
   const innerWidth = width - margin.left - margin.right;
   const innerHeight = height - margin.top - margin.bottom;
 
-  // Bar dimensions
-  const barWidth = innerWidth * 12 / data.length;
-  const barHeight = innerHeight / 12;
+  // Cell dimensions
+  const cellWidth = innerWidth * 12 / data.length;
+  const cellHeight = innerHeight / 12;
 
-  
-  
-
-  
-
-  // x scale
+  // X scale
   const xScale = scaleTime()
     .domain(extent(data, xValue))
-    .range([0, innerWidth]);
-
-  console.log(xScale(1760));
-
-  // x axis
+    .range([0, innerWidth]);  
+  
+  // X axis
   const xAxis = axisBottom(xScale)
     .tickFormat(format(''))
-    //.tickSize(-innerHeight)
     .tickPadding(15);  
 
-  // y scale
+  // Y scale
   const yScale = scaleBand()
-    .domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-    .range([innerHeight, 0]);
+  .domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+  .range([innerHeight, 0]);
 
-  // Get month name
+  // Get month name from a number
   const yAxisTickFormat = monthNumber => {
     const parseMonth = timeParse('%m');
     const month = parseMonth(monthNumber);
@@ -82,52 +74,45 @@ const render = (sourceData) => {
     .append('g')
     .attr('transform', `translate(${margin.left},${margin.top})`);
 
-  // Title
-  container.append('text')
-    .attr('id', 'title')
-    .attr('y', -60)
-    .text('Monthly Global Land-Surface Temperature');
-
-  // Sub title
-  container.append('text')
-    .attr('id', 'sub-title')
-    .attr('y', -30)
-    .text('1753 - 2015: base temperature 8.66℃');
-
-  // Y axis
-  const yAxisG = container.append('g').call(yAxis)
+  // Append y axis
+  container.append('g').call(yAxis)
     .attr('id', 'y-axis')
-  
-  // Remove y axis domain line
-  yAxisG.select('.domain').remove();
+    .attr('class', 'axis')
+      // Remove domain line
+      .select('.domain').remove();  
 
-  // y axis label
-  yAxisG.append('text')
-    .attr('id', 'y-axis-label')
-    .attr('text-anchor', 'middle')
-    .attr('x', -innerHeight / 2)
-    .attr('y', -50)
-    .attr('transform', 'rotate(-90)')
-    .text('GDP in Billions of Dollars');
-
-  // X axis
-  const xAxisG = container.append('g').call(xAxis)
+  // Append x axis
+  container.append('g').call(xAxis)
     .attr('transform', `translate(0,${innerHeight})`)
     .attr('id', 'x-axis')
-  
-  // Remove x axis domain line
-  xAxisG.select('.domain').remove();
+    .attr('class', 'axis')
+      // Remove domain line
+      .select('.domain').remove();
 
-  // Bars
+  // Cells
   container.selectAll('rect').data(data).enter()
     .append('rect')
-      .attr('class', 'bar')
+      .attr('class', 'cell')
       // Dimensions
-      .attr('width', barWidth)
-      .attr('height', barHeight)
+      .attr('width', cellWidth)
+      .attr('height', cellHeight)
       // Position
       .attr('x', (d) => xScale(xValue(d)))
-      .attr('y', d => yValue(d) * barHeight)
+      .attr('y', d => yValue(d) * cellHeight);
+
+    // Title
+  container.append('text')
+  .attr('id', 'title')
+  .attr('class', 'description')
+  .attr('y', -60)
+  .text('Monthly Global Land-Surface Temperature');
+
+  // Description
+  container.append('text')
+    .attr('id', 'description')
+    .attr('class', 'description')
+    .attr('y', -30)
+    .text('1753 - 2015: base temperature 8.66℃');
 };
 
 // Fetch data
